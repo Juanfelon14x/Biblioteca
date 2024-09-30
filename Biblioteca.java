aeea7618b3741e2e6f8f24dae33c0cffa7f20ec7
andrewandfer <fernelyandres13@gmail.com>
2024-09-29 19:25:35 -0500

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -98,4 +98,101 @@ public class Biblioteca {
     public void setCantidadLibros(int cantidadLibros) {
         this.cantidadLibros = cantidadLibros;
     }
-  
+  /**
+     * toString de la clase biblioteca, en donde, en lugar de mostrar cada lista de sus atributos, muestra
+     * la cantidad de elementos de estas, por cuestion de pruebas y con el fin de evitar un error ciclico
+     * que muestra los las listas anidadas de las listas de forma indefinida
+     */
+    @Override
+    public String toString() {
+        return "\n\nBiblioteca [nombre=" + nombre + ", estudiantes=" + estudiantes.size() + ", bibliotecarios=" + bibliotecarios.size()
+                + ", prestamos=" + prestamos.size() + ", libros=" + libros.size() + ", ganancia=" + ganancia + ", cantidadLibros="
+                + cantidadLibros + "]";
+    }
+//---------------------------------------------------Libros--------------------------------------------------------//
+//----------------------------------Gestion global-----------------------------------//
+
+    /**
+     * Metodo para verificar la existencia de un libro en la lista de libros de la biblioteca
+     * - si no hay un libro con el mismo codigo que el ingresado el centinela es false
+     * - si hay un libro con el mismo codigo que el ingresado el centinela es true
+     * @param codigo codigo del libro a verificar
+     * @return el centinela de tipo booleano
+     */
+    public boolean verificarLibro(String codigo) {
+        boolean centinela = false;
+        for (Libro libro : libros) {
+            if (libro.getCodigo().equals(codigo)) {
+                centinela = true;
+            }
+        }
+        return centinela;
+    }
+
+    /**
+     * Metodo para agregar un libro ingresado si no existe en la lista de libros de la biblioteca
+     * @param libro un libro que no esté en la base de datos de la biblioteca
+     * @see #reemplazarLibro(Libro, Libro)
+     */
+    public void agregarLibro(Libro libro) {
+        if (verificarLibro(libro.getCodigo()) == false) {
+            libros.add(libro);
+            setCantidadLibros(getCantidadLibros()+1);
+        }
+    }
+    /**
+     * Metodo para eliminar un libro ingresado si existe en la lista de libros de la biblioteca
+     * @param libro un libro que esté en la base de datos de la biblioteca
+     */
+    public void eliminarLibro(Libro libro) {
+        if (verificarLibro(libro.getCodigo()) == true) {
+            libros.remove(libro);
+            setCantidadLibros(getCantidadLibros()-1);
+        }
+    }
+//----------------------------------Gestion parcial-----------------------------------//
+    /**
+    * 3.2
+    * Mostrar los datos de un libro dado su codigo
+    * @param codigoelegido codigo a consultar
+    * @see #mostrarprestamoslibro(String)
+    */
+    public void mostrardatoslibro(String codigoelegido){
+        String mensaje = "";
+        for (Libro libro : libros) {
+            if (libro.getCodigo().equals(codigoelegido)) {
+                mensaje = libro.toString();
+                break;
+            }
+        }
+        System.out.println(mensaje);
+    }
+
+    /**
+    * 3.3
+    * Mostrar el numero de prestamos en los que está involucrado un libro dado su titulo
+    * @param tituloelegido titulo del libro a consultar
+    * @see #mostrardatoslibro(String)
+    */
+    public void mostrarprestamoslibro(String tituloelegido){
+        int contador = 0;
+        for (Prestamo prestamo : prestamos) {
+            for (DetallePrestamo detallePrestamo : prestamo.getDetallePrestamos()) {
+                if (detallePrestamo.getLibro().getTitulo().equals(tituloelegido)) {
+                    contador++;
+                }
+            }
+        }
+        System.out.println("El libro de titulo " + tituloelegido + " está en " + contador + " prestamos\n");
+    }
+    
+    /**
+     * 3.4
+     * Metodo alternativo para agregar un libro, sobreescribiendolo en la posicion de otro
+     * @param libronuevo un libro que no esté en la base de datos de la biblioteca
+     * @param libroacambiar un libro existente a intercambiar
+     * @see #agregarLibro(Libro)
+     */
+    public void reemplazarLibro(Libro libronuevo, Libro libroacambiar){
+        libros.set(libros.indexOf(libroacambiar), libronuevo);
+    }
